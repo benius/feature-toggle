f7907c4bfa4d51c14c01609afecc20fc7065acf4
Mason Hsieh <benius@gmail.com>
2023-04-12 17:12:57 +0800

    Initial commit

---
+++ src/main/java/com/benius3/featuretoggle/FeatureToggleApplication.java
@@ -0,0 +1,13 @@
+package com.benius3.featuretoggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureToggleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benius3/featuretoggle/FeatureToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benius3.featuretoggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
